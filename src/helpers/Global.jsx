662e5a8f02b061ef7d51371beda89eaf92ceca68
defaaldi <defaaldi@gmail.com>
2024-11-14 10:47:02 -0500

    Final-Build_Proyect_Social_Network_B-270

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,15 +1,11 @@
 //Ruta del Backend desplegado
-// export const Global = {
-// url: "https://b3-backend-social-net.onrender.com/api/",
-// };
+export const Global = {
+url: "https://backend-social-net-0vxt.onrender.com/api/",
+};
 
-// Ruta del Backend desplegado
-//export const Global = {
-//  url: "https://backend-social-net-0vxt.onrender.com/api/",
-//};
 
 // Ruta del Backend en Local: se debe ejecutar el servidor del backend
 
-export const Global = {
-  url: "http://localhost:3900/api/",
-};
+// export const Global = {
+//   url: "http://localhost:3900/api/",
+// };
